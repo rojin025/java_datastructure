1516c2ae58c7d297499ec18aa2fc363d50da0ed4
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-03-01 21:57:19 -0600

    hash and linkedlist

---
+++ src/main/java/com/example/demo/dataStructure/HashMapExample.java
@@ -0,0 +1,29 @@
+package com.example.demo.dataStructure;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    // static HashMap<String, Integer> basket = new HashMap<>();
+
+    public static void main (String[] args){
+        HashMap<String, Integer> basket = new HashMap<>();
+
+        basket.put("apple", 1);
+        System.out.println(basket.get("apple"));
+        
+        basket.put("apple", 3);
+        System.out.println(basket.get("apple"));
+        
+        basket.put("orange", 1);
+        System.out.println(basket.containsKey("orange"));
+        
+        basket.merge("apple", 1, Integer::sum);
+        System.out.println(basket.get("apple"));
+        
+        basket.merge("orange",2,Integer::sum);
+        System.out.println(basket.get("orange"));
+        
+    }
+
+}
---
+++ src/main/java/com/example/demo/dataStructure/LinkedListExample.java
@@ -0,0 +1,27 @@
+package com.example.demo.dataStructure;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args){
+
+        LinkedList<String> list= new LinkedList<>();
+        list.add("First item");
+        // list.add("Sec item");
+        // list.add("Third item");
+
+        System.out.println("List:" + list);
+
+        System.out.println("Getfirst: " + list.getFirst());
+        System.out.println("GetLast: " + list.getLast());
+
+        //  System.out.println("Poll: " + list.poll());
+        System.out.println("Pop: " + list.pop());
+        System.out.println("Poll: " + list);
+        
+        // returns null if []
+        System.out.println("Pop: " + list.poll()); 
+    }
+
+}
